4acabbfb1a07ee4212d0883b463a3d9b08f80558
Dackefrsik <danielfrisk21@gmail.com>
2025-03-06 21:37:00 +0100

    Påbörjat profil
    Har påbörjat byggandet av min profil

--- cv-webbapp/src/body/Home.jsx
+++ cv-webbapp/src/body/Home.jsx
@@ -1,10 +1,64 @@
 
+import { useEffect, useState } from "react";
+import profilbild from "../assets/Profilbild.jpg";
 
-function Home(){
-    
+function Home() {
+
+    const [currentDate, setCurrentDate] = useState("");
+
+    //#region Funkltion som räknar ut ålder
+    function calculateAge() {
+        let birthYear = 2000;
+        let birthMonth = 8;
+        let birthDate = 24;
+        let currentYear = new Date().getFullYear();
+        let currentMonth = new Date().getMonth();
+        let currentDate = new Date().getDay();
+
+        let age;
+
+        if (currentMonth >= birthMonth && birthDate >= currentDate) {
+            age = currentYear - birthYear;
+        }
+        else {
+            age = currentYear - birthYear - 1;
+        }
+
+        setCurrentDate(age);
+    }
+
+    useEffect(() => {
+        calculateAge();
+    })
+    //#endregion
+
     return (
         <>
             <h1 id="Home">Home</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <img src={profilbild} alt="Profilbild" className="w-75 h-75 rounded-circle" />
+                    </div>
+                    <div className="col-8 pt-5">
+                        <div className="row mt-5">
+                            <div className="col-1 fontHome">Namn</div>
+                            <div className="col-2 fontHome">Daniel Frisk</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-1 fontHome">Ålder</div>
+                            <div className="col-2 fontHome">{currentDate}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-1 fontHome">Stad</div>
+                            <div className="col-2 fontHome">Karlstad</div>
+                        </div>
+                        <div className="row">
+                            <div className="col fontHome">Webbutvecklarstudent på Handelshögskolan vid Karlstads universitet</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
