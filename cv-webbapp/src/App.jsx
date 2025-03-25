48929ffe34e3d1b9aaccff186ce93e29b32055f0
Dackefrsik <danielfrisk21@gmail.com>
2025-03-25 19:52:55 +0100

    Byggt en footer
    Lagt till en footer som visar vilka språk/ tekniker som används för att skapa sidan och vad som används för att hoasta sidan

--- cv-webbapp/src/App.jsx
+++ cv-webbapp/src/App.jsx
@@ -1,6 +1,7 @@
 
 import Navbar from "./mainParts/Navbar"
 import Body from "./mainParts/Body"
+import Footer from "./mainParts/Footer"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Body />
+      <Footer/>
     </>
   )
 }
