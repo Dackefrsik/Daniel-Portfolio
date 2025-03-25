58b840c094693e0dcb1bd59c4e96712ce3a95133
Dackefrsik <danielfrisk21@gmail.com>
2025-03-25 20:54:24 +0100

    Animation projekt
    Har satt en liten animation till bilderna i projektkomponenten som tar deras opacitet från 0 till 1 när de kommer in i bild

--- cv-webbapp/src/body/Projects.jsx
+++ cv-webbapp/src/body/Projects.jsx
@@ -1,6 +1,36 @@
+import { useEffect } from "react"
 
-
+//Komponent som visar projekt
 function Projects() {
+
+    /*#region useefect som kör animationer*/
+    useEffect(() => {
+
+        //Hämtar ut alla bilder i komponenten
+        let imgRef = document.querySelectorAll("img");
+
+
+        //IntersectObserver som visar bilderna genom att sätta opaciteten till 1
+        const observer = new IntersectionObserver(entries => {
+
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("opacityAfter")
+                }
+            },
+                {
+                    //Görs när halva bilden visas
+                    threshold: 0.5
+                })
+        })
+
+        //Lopar igenom alla bilder och observerar dem
+        imgRef.forEach(img => {
+            observer.observe(img);
+        })
+
+    }, [])
+
 
 
     return (
@@ -9,17 +39,12 @@ function Projects() {
                 <div className="col-12 col-md-4 bgProfilePicture d-flex justify-content-center">
                     <p className="textColor">Projects</p>
                 </div>
-                <div className="col-1 bgProfilePicture"></div>
-                <div className="col-7 bgProfilePicture"></div>
-            </div>
-            <div className="row">
-                <div className="col-md-4 bgProfilePicture"></div>
                 <div className="col-md-1 bgProfilePicture"></div>
                 <div className="d-none d-md-block col-md-7 bgProfilePicture p-md-2">
-                    <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" className="imgProjects programingImagePosition me-1" />
-                    <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3" className="imgProjects programingImagePosition me-1" />
-                    <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap" className="imgProjects programingImagePosition me-1" />
-                    <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript" className="imgProjects programingImagePosition" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt="HTML5" className="imgProjects programingImagePosition opacityBefore" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt="CSS3" className="imgProjects programingImagePosition opacityBefore" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" alt="Bootstrap" className="imgProjects programingImagePosition opacityBefore" />
+                    <img src="https://camo.githubusercontent.com/34c801e15a80d0d62f3da09bc6ca4f46a243457939381ae67f5003bdac51d432/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6a6176617363726970742f6a6176617363726970742d706c61696e2e737667" alt="JavaScript" className="imgProjects programingImagePosition opacityBefore" />
                 </div>
             </div>
             <div className="row">
@@ -28,7 +53,7 @@ function Projects() {
                 <div className="col-12 col-md-7 bgProfilePicture p-md-2 d-flex d-md-block justify-content-center">
                     <div>
                         <a href="https://github.com/Dackefrsik/Weatherapp" target="_blank" className="mb-2">
-                            <img alt="Weather application" className="img-fluid mb-2 mb-md-0" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Weatherapp&theme=cobalt2"></img>
+                            <img alt="Weather application" className="img-fluid mb-2 mb-md-0 opacityBefore" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Weatherapp&theme=cobalt2"></img>
                         </a>
                     </div>
                 </div>
@@ -39,7 +64,7 @@ function Projects() {
                 <div className="col-12 col-md-7 bgProfilePicture p-md-2 d-flex d-md-block justify-content-center">
                     <div>
                         <a href="https://github.com/Dackefrsik/Tennis-booking" target="_blank" className="mb-2">
-                            <img alt="Tennis-booking" className="img-fluid mb-2 mb-md-0" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Tennis-booking&theme=cobalt2"></img>
+                            <img alt="Tennis-booking" className="img-fluid mb-2 mb-md-0 opacityBefore" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Tennis-booking&theme=cobalt2"></img>
                         </a>
                     </div>
                 </div>
@@ -48,7 +73,7 @@ function Projects() {
                 <div className="col-md-4 bgProfilePicture"></div>
                 <div className="col-md-1 bgProfilePicture"></div>
                 <div className="d-none d-md-block col-md-7 bgProfilePicture p-md-2">
-                    <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React" className="imgProjects programingImagePosition me-2" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" className="imgProjects programingImagePosition me-2 opacityBefore" />
                 </div>
             </div>
             <div className="row">
@@ -57,7 +82,7 @@ function Projects() {
                 <div className="col-12 col-md-7 bgProfilePicture p-md-2 d-flex d-md-block justify-content-center">
                     <div>
                         <a href="https://github.com/Dackefrsik/Tournament-webbapp" target="_blank" className="mb-2">
-                            <img alt="Tournament-webbapp" className="img-fluid mb-2 mb-md-0" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Tournament-webbapp&theme=cobalt2"></img>
+                            <img alt="Tournament-webbapp" className="img-fluid mb-2 mb-md-0 opacityBefore" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Tournament-webbapp&theme=cobalt2"></img>
                         </a>
                     </div>
 
@@ -67,7 +92,7 @@ function Projects() {
                 <div className="col-4 bgProfilePicture"></div>
                 <div className="col-1 bgProfilePicture"></div>
                 <div className="d-none d-md-block col-md-7 bgProfilePicture p-md-2">
-                    <img src="https://img.shields.io/badge/Java-F8981D?style=for-the-badge&logo=java&logoColor=white" alt="Java" className="imgProjects programingImagePosition me-2" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Java" className="imgProjects programingImagePosition me-2 opacityBefore" />
                 </div>
             </div>
             <div className="row">
@@ -76,7 +101,7 @@ function Projects() {
                 <div className="col-12 col-md-block col-md-7 bgProfilePicture p-md-2 d-flex d-md-block justify-content-center">
                     <div className="">
                         <a href="https://github.com/Dackefrsik/Snake_game" target="_blank" className="mb-2">
-                            <img alt="Snake_game" className="img-fluid mb-2 mb-md-0" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Snake_game&theme=cobalt2"></img>
+                            <img alt="Snake_game" className="img-fluid mb-2 mb-md-0 opacityBefore" src="https://github-readme-stats.vercel.app/api/pin/?username=Dackefrsik&repo=Snake_game&theme=cobalt2"></img>
                         </a>
                     </div>
 
