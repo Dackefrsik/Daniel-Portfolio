860be992b58d4c4501098bbd0c32709b1fa60861
Dackefrsik <danielfrisk21@gmail.com>
2025-04-07 21:56:18 +0200

    Beteende i navbar
    Har ändrat beteendet i navigeringslisten, nu stänger den sig på små skärmar om någon klickar på den

--- cv-webbapp/src/mainParts/Navbar.jsx
+++ cv-webbapp/src/mainParts/Navbar.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useRef } from "react";
 
 function Navbar() {
+
+    //useEffect för att hantera klassen show på diven collapse
+    const collapseRef = useRef(null);
+
+    //#region useEffect för att ta bort klassen show när användaren klickar på en av länkarna
+    useEffect(() => {
+        let aRef = document.querySelectorAll("a");
+
+        aRef.forEach(a => {
+            a.addEventListener("click", () => {
+                collapseRef.current.classList.remove("show");
+            })
+        })
+    })
+    //#endregion
+
 
     return (
         <>
@@ -9,13 +26,11 @@ function Navbar() {
                         <button className="custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup" ref={collapseRef}>
                             <div className="navbar-nav">
-                                <a className="active m-2" aria-current="page" href="#Home">Home</a>
+                                <a className="active m-2" aria-current="page" href="#Home" >Home</a>
                                 <a className="m-2" href="#Tekniks">ProgrammingTekniks</a>
                                 <a className="m-2" href="#Projects">Projects</a>
-                                <a className="m-2" href="#Worklife">Worklife</a>
-                                <a className="m-2" href="#School">School</a>
                                 <a className="m-2" href="#Hobie">Hobies</a>
                             </div>
                         </div>
