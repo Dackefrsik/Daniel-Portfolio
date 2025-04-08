1419545aa945bcb9eefa78f59d6039fdbf924515
Dackefrsik <danifris102@student.kau.se>
2025-04-08 17:30:20 +0200

    Navbar-toggler
    Har ändrat positionen på togglern så den ligger längs högerkanten

--- cv-webbapp/src/mainParts/Navbar.jsx
+++ cv-webbapp/src/mainParts/Navbar.jsx
@@ -37,15 +37,15 @@ function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-md fixed-top">
-                <div className="container-fluid d-flex justify-content-end">
-                    <div className="d-flex align-items-start d-flex justify-content-center">
-                        <button className={`ps-1 pe-1 pt-1 pb-1 custom-toggler ${isRounded ? "border-right-radius" : ""}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" ref={menuButton} onClick={openMenu}>
+                <div className="w-100 d-md-block d-flex justify-content-end">
+                    <div className="d-flex align-items-start justify-content-end">
+                        <button className={`ms-5 ps-1 pe-1 pt-1 pb-1 custom-toggler ${isRounded ? "border-right-radius" : ""}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" ref={menuButton} onClick={openMenu}>
                             <span className="navbar-toggler-top"></span>
                             <span className="navbar-toggler-middle"></span>
                             <span className="navbar-toggler-bottom"></span>
                         </button>
                         <div className={`collapse navbar-collapse ${isMenueOpen ? "Show" : ""} `} id="navbarNavAltMarkup" ref={collapseRef}>
-                            <div className="navbar-nav">
+                            <div className="ms-md-5 navbar-nav">
                                 <a className="active m-2" aria-current="page" href="#Home" onClick={closeMenue}>Home</a>
                                 <a className="m-2" href="#Tekniks" onClick={closeMenue}>ProgrammingTekniks</a>
                                 <a className="m-2" href="#Projects" onClick={closeMenue}>Projects</a>
