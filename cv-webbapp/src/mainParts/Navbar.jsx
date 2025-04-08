cdeae97220e441b657e776a633719139a29fa568
Dackefrsik <danifris102@student.kau.se>
2025-04-08 10:27:23 +0200

    Uppdaterade effekter på min navbar
    Gått från mer js syntax till react syntax

--- cv-webbapp/src/mainParts/Navbar.jsx
+++ cv-webbapp/src/mainParts/Navbar.jsx
@@ -1,37 +1,50 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Navbar() {
 
     //useEffect för att hantera klassen show på diven collapse
     const collapseRef = useRef(null);
 
-    //#region useEffect för att ta bort klassen show när användaren klickar på en av länkarna
-    useEffect(() => {
-        let aRef = document.querySelectorAll("a");
+    //useState för höger border-radius
+    const[isRounded, steClass] = useState(true);
 
-        aRef.forEach(a => {
-            a.addEventListener("click", () => {
-                collapseRef.current.classList.remove("show");
-            })
-        })
-    })
+    //useState för att kolla om menyn är öppen
+    const[isMenueOpen, setMenueOpen] = useState(false);
+
+    //#region Funktioner för att öppna och stännga hamburgermenyn
+    const openMenu = () => {
+        if(isRounded == true){
+            steClass(false);
+            setMenueOpen(true);
+        }
+        else if(isRounded == false){
+            steClass(true);
+            setMenueOpen(false);
+        }
+    }
+
+    const closeMenue = () => {
+        if(isMenueOpen == true){
+            setMenueOpen(false);
+            steClass(true);
+        }
+    }
     //#endregion
-
 
     return (
         <>
             <nav className="navbar navbar-expand-md fixed-top">
                 <div className="container-fluid d-flex justify-content-end">
                     <div className="d-flex align-items-start d-flex justify-content-center">
-                        <button className="custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className={`custom-toggler ${isRounded ? "border-right-radius" : ""}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" onClick={openMenu}>
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup" ref={collapseRef}>
+                        <div className={`collapse navbar-collapse ${isMenueOpen ? "Show" : ""} `} id="navbarNavAltMarkup" ref={collapseRef}>
                             <div className="navbar-nav">
-                                <a className="active m-2" aria-current="page" href="#Home" >Home</a>
-                                <a className="m-2" href="#Tekniks">ProgrammingTekniks</a>
-                                <a className="m-2" href="#Projects">Projects</a>
-                                <a className="m-2" href="#Hobie">Hobies</a>
+                                <a className="active m-2" aria-current="page" href="#Home" onClick={closeMenue}>Home</a>
+                                <a className="m-2" href="#Tekniks" onClick={closeMenue}>ProgrammingTekniks</a>
+                                <a className="m-2" href="#Projects" onClick={closeMenue}>Projects</a>
+                                <a className="m-2" href="#Hobie" onClick={closeMenue}>Hobies</a>
                             </div>
                         </div>
                     </div>
